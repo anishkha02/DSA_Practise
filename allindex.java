dde614c7a26c39787f320e99405bd13e7d50760d
anishkha02 <anishkha0208@gmail.com>
2023-07-12 14:24:56 +0530

    Java code

---
+++ allindex.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class allindex {
+
+    public static int[] all(int []a,int x,int i,int c)
+    {
+        if(i==a.length)
+        {
+            return new int[c];
+
+        }
+        else if(a[i]==x)
+        {
+            int []fs=all(a,x,i+1,c+1);
+            a[c]=i;
+            return fs;
+        }
+        else
+        {
+            int []fs=all(a,x,i+1,c);
+            return fs;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scn.nextInt();
+        }
+        int key = scn.nextInt();
+        int[] fs = all(a, key, 0, 0);
+    }
+}
