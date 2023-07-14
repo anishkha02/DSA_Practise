a35bd47eb2b8cd7b56fe6a266f3888a5bea17903
anishkha02 <anishkha0208@gmail.com>
2023-07-14 11:34:31 +0530

    java codes

--- getkpc.java
+++ getkpc.java
@@ -4,12 +4,12 @@ public class getkpc {
     {
         Scanner scn=new Scanner(System.in);
         String shreya=scn.nextLine();
-        ArrayList<String>ans=getkpc(shreya);
+        ArrayList<String>ans=Getkpc(shreya);
         System.out.println(ans);
     }
     //static String[]a={" ","abc","def","ghi","jkl","tu","zw"};
     static String[]a={" ","ab","cd","ef","gh","ijk","lmn"};
-    public static ArrayList<String>getkpc(String str){
+    public static ArrayList<String>Getkpc(String str){
         if(str.length()==0)
         {
             ArrayList<String>base=new ArrayList<>();
@@ -18,7 +18,7 @@ public class getkpc {
         }
         char ch=str.charAt(0);
         String ros=str.substring(1);
-        ArrayList<String>rr=getkpc(ros);
+        ArrayList<String>rr=Getkpc(ros);
         String code=a[ch-'0'];
         ArrayList<String >myans=new ArrayList<>();
         for(int i=0;i<code.length();i++)
